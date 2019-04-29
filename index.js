55a6fbf5c9609136f06d13701dded94365824808
soer145a <soren.remboll@gmail.com>
2019-04-29 10:26:00 +0100

    Fuck adobe illustrator

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 window.addEventListener("DOMContentLoaded", init);
 function init() {
   console.log("INIT");
-  fetch("timeline-mockup2-01.svg")
+  fetch("timeline-mockup2.svg")
     .then(response => response.text())
     .then(svgData => {
       console.log("The SVG data:", svgData);
-      document.querySelector("#svg_timeline").innerHTML = svgData;
+      document
+        .querySelector("#svg_timeline")
+        .insertAdjacentHTML("afterbegin", svgData);
+      fitRect("#philosopher .HTML_placeholder", "#movie_1");
     });
+}
+
+function fitRect(svgElement, htmlElement) {
+  svgElement = document.querySelector(svgElement);
+  htmlElement = document.querySelector(htmlElement);
+
+  htmlElement.style.left = svgElement.getAttribute("x") + "px";
+  htmlElement.style.top = svgElement.getAttribute("y") + "px";
+  htmlElement.style.width = svgElement.getAttribute("width") + "px";
+  htmlElement.style.height = svgElement.getAttribute("height") + "px";
 }
