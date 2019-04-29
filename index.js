2cef48d6e074020144fe8365c08ac14f36062b63
soer145a <soren.remboll@gmail.com>
2019-04-29 09:33:09 +0100

    Added svg

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 window.addEventListener("DOMContentLoaded", init);
 function init() {
   console.log("INIT");
+  fetch("timeline-mockup2-01.svg")
+    .then(response => response.text())
+    .then(svgData => {
+      console.log("The SVG data:", svgData);
+      document.querySelector("#svg_timeline").innerHTML = svgData;
+    });
 }
