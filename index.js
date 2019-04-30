5017c0614a17380a8775899c60082321e421cfe6
soer145a <soren.remboll@gmail.com>
2019-04-30 10:47:59 +0100

    It

--- index.js
+++ index.js
@@ -1,17 +1,43 @@
+window.addEventListener("DOMContentLoaded", loadJson);
 window.addEventListener("DOMContentLoaded", init);
+
 window.addEventListener("resize", resizeSVG);
 function init() {
   console.log("INIT");
   fetch("timeline-mockup2.svg")
     .then(response => response.text())
     .then(svgData => {
-      console.log("The SVG data:", svgData);
       document
         .querySelector("#svg_timeline")
         .insertAdjacentHTML("afterbegin", svgData);
 
       resizeSVG();
     });
+}
+function loadJson() {
+  fetch("data.json")
+    .then(response => response.json())
+    .then(jsonData => {
+      console.log(jsonData);
+      jsonData.forEach(handler);
+    });
+}
+function handler(object) {
+  console.log(object);
+  let movieTemplate = document.querySelector("#movieTemplate");
+  let clonedTemplate = movieTemplate.content.cloneNode(true);
+  clonedTemplate.querySelector("[data-field='title']").textContent =
+    object.title;
+  clonedTemplate.querySelector("[data-field='year']").textContent = object.year;
+  clonedTemplate.querySelector("[data-field='director']").textContent =
+    object.director;
+  clonedTemplate.querySelector("[data-field='img']").src =
+    object.image + ".jpg";
+
+  console.log(`.svgplaceholder [data-svgplaceholder=${object.id}]`);
+  document
+    .querySelector(`.svgplaceholder[data-svgplaceholder=${object.id}]`)
+    .append(clonedTemplate);
 }
 
 function resizeSVG() {
@@ -22,7 +48,6 @@ function resizeSVG() {
 function fitRect(svgElement, htmlElement) {
   svgElement = document.querySelector(svgElement);
   const rect = svgElement.getBoundingClientRect();
-  console.log(rect);
 
   htmlElement.style.left = rect.x + "px";
   htmlElement.style.top = rect.y + "px";
